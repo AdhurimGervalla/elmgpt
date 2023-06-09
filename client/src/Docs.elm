17e0d77d0b01c76f3a89cdb5764c9269cc985499
Adhurim Gervalla <adi@hausformat.com>
2023-06-09 16:27:34 +0200

    Revert "Styling changes"
    This reverts commit 2f68f0edad6a8f3e09726c828126d73167b857a4.

--- client/src/Docs.elm
+++ client/src/Docs.elm
@@ -11,14 +11,6 @@ import Http exposing (Error)
 import Decoders exposing (..)
 import Encoders exposing (..)
 
-choices : List Message
-choices =
-    [ {role = User, content = "Hello Worldddddddddddddddddddddddddddddddddddddddddddddddddddddddd ddddddddddddddddddddddd dddddddddddddddddddddddddddddddddd dddddddddddddddddddddddddddd Hello Worldddddddddddddddddddddddddddddddddddddddddddddddddddddddd"}
-    , {role = Assistant, content = "Hello how can I help you?"}
-    , {role = User, content = "Serve me a drink"}
-    , {role = Assistant, content = "Sure son"}
-    ]
-
 view : Model -> Html Msg
 view model =
     let
@@ -28,7 +20,6 @@ view model =
           [
           viewFilterInput model,
           styled div [displayFlex, flexWrap wrap] [] (List.map (\content -> viewDocsItem content) filteredItems),
-          a [href "/docs/issue-id/eh5knmlwex3nils"] [text "Click here"],
           appFooter
           ]
 
@@ -58,19 +49,16 @@ viewDetail slug model =
     styled div [margin (px 0)] []
   [ 
     
-  styled div [ marginLeft (px 100), marginRight auto, marginTop (px 100), marginRight (px 100)] [] [
+  styled div [ marginLeft (px 100), marginRight auto, marginTop (px 100), maxWidth (px 800)] [] [
       
-        --ul [] (List.map viewMessage model.detailPage.messages)
-        ul [] (List.map viewMessage choices)
+        ul [] (List.map viewMessage model.detailPage.messages)
   ]
   , appFooter
   ]
 
-
 viewMessage : Message -> Html Msg
 viewMessage message =
-    styled div [boxShadow5 (px 1) (px 1) (px 4) (px 4) (rgba 150 150 150 0.2), marginTop (px 20), 
-    if message.role == User then backgroundColor (rgb 255 255 255) else backgroundColor (rgb 250 250 250)] [] [styled div (if message.role == User then userStyled else assistantStyled) [] [ text message.content ],
+    div [] [styled li [if message.role == User then fontWeight bold else fontWeight normal] [] [ text message.content ],
             p [] []]
 
 getOne : String -> Cmd Msg
@@ -82,9 +70,3 @@ getOne id =
             url = uri,
             expect = Http.expectJson GotResponseFromOnePocketbase decodeApiResponsePocketbase
         } 
-
-userStyled : List Style
-userStyled = [marginLeft (px 20), paddingTop (px 20), paddingBottom (px 20), color (rgb 50 50 50)]
-
-assistantStyled : List Style
-assistantStyled = [fontWeight normal, marginLeft (px 20), paddingTop (px 20), paddingBottom (px 20)]
